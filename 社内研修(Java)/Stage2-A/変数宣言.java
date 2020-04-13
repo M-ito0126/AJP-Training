2c62e7c746f6c8457c04c095d28346c4e1a68390
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-13 15:18:45 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-A/変数宣言.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String []args) {
+        String str = "12";
+        int eleven = Integer.parseInt(str);
+        int five = 5;
+        int total = eleven * five;
+        
+        System.out.println(total);
+    }
+}
