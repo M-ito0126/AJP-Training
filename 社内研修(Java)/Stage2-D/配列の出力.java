872656d5ed0e6e23bd2ede425a5f1db57846e6c1
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-14 17:58:54 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-D/配列の出力.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String []args) {
+        String str;
+        
+        String[] fruit = {"orange", "apple", "cherry", "grape", "apple"};
+        
+        System.out.println("１：" + fruit[0]);
+        System.out.println("２：" +  fruit[1]);
+        System.out.println("３：" +  fruit[2]);
+        System.out.println("４：" +  fruit[3]);
+        System.out.println("５：" +  fruit[4]);
+    }
+}
