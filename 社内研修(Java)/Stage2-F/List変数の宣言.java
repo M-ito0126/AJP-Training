69c1fdadebcf77041b7f1d223663e0bda5998915
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-16 16:25:08 +0900

    2回目提出

--- 社内研修(Java)/Stage2-F/List変数の宣言.java
+++ 社内研修(Java)/Stage2-F/List変数の宣言.java
@@ -7,9 +7,5 @@ public class Week {
         List<String> list = new ArrayList<String>();
         
         Collections.addAll(list, "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
-        //文字列型の変数weekにlistの値を代入   
-       for(String week:list) {
-           System.out.println(week);
-       }
     }
 }
