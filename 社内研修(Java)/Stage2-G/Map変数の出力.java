0d472cb6a461beb3f9c7a5a5539c2e5045f66491
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-17 15:42:23 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-G/Map変数の出力.java
@@ -0,0 +1,24 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Iterator;
+
+public class WeekDays {
+    public static void main(String []args) {
+        List<String> listAlphabet = new ArrayList<>();
+        Collections.addAll(listAlphabet, "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
+        List<String> listJapanese = new ArrayList<>();
+        Collections.addAll(listJapanese, "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日");
+        Map<String, String> map = new LinkedHashMap<>();
+        //listAlphabetとlistJapaneseをmapに追加
+        for (int i =0; i < listAlphabet.size(); i++) {
+            map.put(listAlphabet.get(i).toUpperCase(), listJapanese.get(i));
+        }
+        //mapを出力する
+        for(Map.Entry<String, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + "（" + entry.getValue() + "）");
+		}
+    }
+}
