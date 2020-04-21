84cbac91d0a318e14bb0cdbcfed2c0ad06835d08
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-21 13:11:22 +0900

    2回目提出

--- 社内研修(Java)/Stage2-E/重複の排除.java
+++ 社内研修(Java)/Stage2-E/重複の排除.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
-public class Duplication {
+public class Samp{
     public static void main(String []args) {
-        String fruit = "orange/apple/cherry/orange/grape/apple";
-        String[] fruitArray = {"orange", "apple", "cherry", "orange", "grape", "apple"};
-        
-        List list = Arrays.asList(fruitArray);
-        if(list.contains("orange") && list.contains("apple")){
-            System.out.println("１：" + fruitArray[0]);
-            System.out.println("２：" + fruitArray[1]);
+        String fruit = "orange,apple,cherry,orange,grape,apple";
+        String[] fruitArray = fruit.split(",");
+        List<String> listF = Arrays.asList(fruitArray);
+        Set<String> set = new HashSet<String>();
+        int i = 0;
+        for(String str : listF) {
+            //setに追加してみて、追加できない値を出力
+            if(!set.add(str)) {
+                i++;
+                System.out.println(i +  "：" + str);
+            }
         }
     }
 }
