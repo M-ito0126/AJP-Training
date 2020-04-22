5b7a9af6434d0687f80b8db46c5cf6b6804c1cb4
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-22 11:05:22 +0900

    初回提出

---
+++ 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
@@ -0,0 +1,27 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+public class VariadicA {
+    public static void main(String []args) {
+        String[] fruit = {"apple", "lemon", "peach"};
+        varA(fruit);
+    }
+    public static String pathName = "/home/ec2-user/environment/サンプル実行/output/20200421_output.txt";
+    private static void varA(String ... f) {
+        
+        try {
+            FileWriter file = new FileWriter(VariadicA.pathName);
+            PrintWriter pw = new PrintWriter(new BufferedWriter(file));
+            //fの要素数の数だけファイルに書き込む
+            for(String fruitF : f) {
+            pw.println(fruitF);
+            }
+            pw.close();
+        }
+        catch (IOException e) {
+        e.printStackTrace();
+        }
+    }
+}
