e158243012620b6fad7d382142cbef0210003f17
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-28 16:04:29 +0900

    4回目提出

--- 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
+++ 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
@@ -2,16 +2,21 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.BufferedWriter;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class VariadicA {
     public static void main(String []args) {
         String[] fruit = {"apple", "lemon", "peach"};
         varA(fruit);
     }
-    public static String pathName = "/home/ec2-user/environment/サンプル実行/output/20200420_output.txt";
+    public static String pathName = "/home/ec2-user/environment/サンプル実行/output/";
     private static void varA(String ... f) {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String fileName = "{" + sdf.format(date) + "}_output.txt";
         try {
-            FileWriter file = new FileWriter(VariadicA.pathName);
+            FileWriter file = new FileWriter(VariadicA.pathName + fileName);
             PrintWriter pw = new PrintWriter(new BufferedWriter(file));
             //fの要素数の数だけファイルに書き込む
             for(String fruitF : f) {
