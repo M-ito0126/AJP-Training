f51477420f960384942ae7be26b20efcb70213d7
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-04-28 14:26:00 +0900

    初回提出

---
+++ 社内研修(Java)/Stage3-E/インスタンスの作成.java
@@ -0,0 +1,58 @@
+import java.io.Serializable;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Userinfo {
+    public class UserBean implements Serializable {
+        private static final long serialVersionUID = 1L;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+        private int id;
+        private String name;
+        private String pass;
+        private Date birthday;
+        private int age;
+        public UserBean() {
+        }
+        public void setProperties(int id, String name, String pass, int age) {
+            this.id = id;
+            this.name = name;
+            this.pass = pass;
+            this.age = age;
+        }
+        public int getId() {
+            return this.id;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public String getPass() {
+            return this.pass;
+        }
+        public Date getBirthday() {
+            return this.birthday;
+        }
+        public int getAge() {
+            return this.age;
+        }
+        public void setBirthday(String birthday) throws ParseException {
+            this.birthday = sdf.parse(birthday);
+        }
+        String br = System.getProperty("line.separator");
+        public String toString() {
+            return "ユーザーID：{" + id + "}" + br + "ユーザー名：{" + name + "}" + br + "パスワード：{" + pass + "}" + br + "生年月日：{" + (sdf.format(birthday)) + "}" + br + "年齢：{" + age + "}";
+        }
+    }
+    public static void main(String []args) {
+        Userinfo ui = new Userinfo();
+        Userinfo.UserBean ub = ui.new UserBean();
+        ub.setProperties(02, "伊藤", "abi2", 21);
+        try {
+            ub.setBirthday("1998年10月24日");
+        }
+        catch(ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.print(ub.toString());
+    }
+}
