8a034eb975ba05bfdc21635274069057d384e339
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-05-08 17:25:02 +0900

    初回提出

---
+++ 社内研修(Java)/Stage3-F/フィールドの追加.java
@@ -0,0 +1,70 @@
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class UserinfoAdd {
+    public static class UserBean implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private static int cnt;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+        private int id;
+        private String name;
+        private String pass;
+        private Date birthday;
+        private int age;
+        private String affiliation;
+        //コンストラクタ
+        public UserBean() {
+        }
+        public UserBean(String affiliation) {
+            this.affiliation = affiliation;
+            UserBean.cnt ++;
+        }
+        //setプロパティ
+        public void setProperties(int id, String name, String pass, int age) {
+            this.id = id;
+            this.name = name;
+            this.pass = pass;
+            this.age = age;
+        }
+        public void setBirthday(String birthday) throws ParseException {
+            this.birthday = sdf.parse(birthday);
+        }
+        //getプロパティ
+        public int getId() {
+            return this.id;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public String getPass() {
+            return this.pass;
+        }
+        public Date getBirthday() {
+            return this.birthday;
+        }
+        public int getAge() {
+            return this.age;
+        }
+        String br = System.getProperty("line.separator");
+        public String toString() {
+            return "ユーザーID：{" + this.getId() + "}" + br + "ユーザー名：{" + this.getName() + "}" + br + "パスワード：{" + this.getPass() + "}"
+                                   + br + "生年月日：{" + (sdf.format(this.getBirthday())) + "}" + br + "年齢：{" + this.getAge() + "}";
+        }
+    }
+    public static void main(String []args) {
+        UserinfoAdd ui = new UserinfoAdd();
+        UserinfoAdd.UserBean ub = new UserBean("Osaka");
+        ub.setProperties(02, "伊藤", "abi2", 21);
+        try {
+            ub.setBirthday("1998年10月24日");
+        }
+        catch(ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println(ub.toString());
+        System.out.println("所属地：{" + ub.affiliation + "}");
+        System.out.println("カウント数：{" + UserBean.cnt + "}");
+    }
+}
