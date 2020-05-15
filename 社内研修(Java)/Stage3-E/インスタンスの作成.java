a365f1c72f5af8652624fbc26f1e8d4dd282de30
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-05-15 11:57:38 +0900

    6回目提出

--- 社内研修(Java)/Stage3-E/インスタンスの作成.java
+++ 社内研修(Java)/Stage3-E/インスタンスの作成.java
@@ -1,9 +1,11 @@
 import java.io.Serializable;
-import java.util.Date;
 import java.text.ParseException;
+import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class Userinfo {
+    //インスタンス生成数をカウントする変数
+    public static int cnt = 0;
     public class UserBean implements Serializable {
         //シリアライズUID
         private static final long serialVersionUID = 1L;
@@ -18,8 +20,26 @@ public class Userinfo {
         private Date birthday;
         //年齢
         private int age;
+        //所属地
+        private String affiliation;
         //コンストラクタ
         public UserBean() {
+        }
+        //setter
+        public void setId(int id) {
+            this.id = id;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public void setPass(String pass) {
+            this.pass = pass;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public void setBirthday(String birthday) throws ParseException {
+            this.birthday = sdf.parse(birthday);
         }
         //getter
         public int getId() {
@@ -31,37 +51,28 @@ public class Userinfo {
         public String getPass() {
             return this.pass;
         }
-        public Date getBirthday() {
-            return this.birthday;
+        public String getBirthday() {
+            return sdf.format(this.birthday);
         }
         public int getAge() {
             return this.age;
-        }
-        //setter
-        public void setProperties(int id, String name, String pass, int age) {
-            this.id = id;
-            this.name = name;
-            this.pass = pass;
-            this.age = age;
-        }
-        public void setBirthday(String birthday) throws ParseException {
-            this.birthday = sdf.parse(birthday);
         }
         //改行コードの取得
         String br = System.getProperty("line.separator");
         @Override
         //toStringメソッドの戻り値に出力したいBeansクラスの各変数と文字列を返す
         public String toString() {
-            return "ユーザーID：{" + this.id + "}" + br + "ユーザー名：{" + this.name + "}" + br+ "パスワード：{" + this.pass + "}" + br 
-                                   + "生年月日：{" + (sdf.format(this.birthday)) + "}" + br + "年齢：{" + this.age + "}";
+            return "ユーザーID：{" + this.id + "}" + br + "ユーザー名：{" + this.name + "}" + br + "パスワード：{" + this.pass + "}"
+                   + br + "生年月日：{" + getBirthday() + "}" + br + "年齢：{" + this.age + "}";
         }
     }
     public static void main(String []args) {
         //外部クラスUserinfoのインスタンスを生成し、内部クラスUserBeanのインスタンスを生成
         UserBean ub = new Userinfo().new UserBean();
-        //UserBeanにユーザID、ユーザ名、パスワード、年齢をセット
-        ub.setProperties(02, "伊藤", "abi2", 21);
-        //UserBeanに生年月日をセット
+        //UserBeanにユーザID、ユーザ名、パスワード、生年月日、年齢をセット
+        ub.setId(02);
+        ub.setName("伊藤");
+        ub.setPass("abi2");
         try {
             ub.setBirthday("1998年10月24日");
         }
@@ -69,6 +80,7 @@ public class Userinfo {
             //スタックトレース（例外内容）を出力
             e.printStackTrace();
         }
-        System.out.print(ub);
+        ub.setAge(21);
+        System.out.println(ub);
     }
 }
