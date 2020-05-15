8cd0a9a5220e3d4cb216c0b3df88c637843cd4c3
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-05-15 10:04:07 +0900

    5回目提出

--- 社内研修(Java)/Stage3-F/フィールドの追加.java
+++ 社内研修(Java)/Stage3-F/フィールドの追加.java
@@ -10,20 +10,25 @@ public class UserinfoAdd {
         //シリアライズUID
         private static final long serialVersionUID = 1L;
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
-        //インスタンスフィールド
+        //ユーザID
         private int id;
+        //ユーザ名
         private String name;
+        //パスワード
         private String pass;
+        //生年月日
         private Date birthday;
+        //年齢
         private int age;
+        //所属地
         private String affiliation;
         //コンストラクタ
         public UserBean() {
         }
         public UserBean(String affiliation) {
             this.affiliation = affiliation;
-            //外部クラスUserinfoの変数cntでUserBeanのコンストラクタが生成されるたびにカウントする
-            UserinfoAdd.this.cnt ++;
+            //外部クラスUserinfoAddの変数cntでUserBeanのコンストラクタが生成されるたびにカウント
+            UserinfoAdd.cnt ++;
         }
         //setter
         public void setProperties(int id, String name, String pass, int age) {
@@ -57,23 +62,29 @@ public class UserinfoAdd {
         public String getAfflication() {
             return this.affiliation;
         }
+        //改行コードの取得
         String br = System.getProperty("line.separator");
+        @Override
+        //toStringメソッドの戻り値に出力したいBeansクラスの各変数と文字列を返す
         public String toString() {
             return "ユーザーID：{" + this.id + "}" + br + "ユーザー名：{" + this.name + "}" + br + "パスワード：{" + this.pass + "}"
-                                   + br + "生年月日：{" + sdf.format(this.birthday) + "}" + br + "年齢：{" + this.age + "}";
+                   + br + "生年月日：{" + sdf.format(this.birthday) + "}" + br + "年齢：{" + this.age + "}"
+                   + br + "所属地：{" + this.affiliation + "}" + br + "カウント数：{" + UserinfoAdd.cnt + "}";
         }
     }
     public static void main(String []args) {
+        //外部クラスUserinfoのインスタンスを生成し、内部クラスUserBeanのインスタンスを生成
         UserBean ub = new UserinfoAdd().new UserBean("Osaka");
+        //UserBeanにユーザID、ユーザ名、パスワード、年齢をセット
         ub.setProperties(02, "伊藤", "abi2", 21);
+        //UserBeanに生年月日をセット
         try {
             ub.setBirthday("1998年10月24日");
         }
         catch(ParseException e) {
+            //スタックトレース（例外内容）を出力
             e.printStackTrace();
         }
-        System.out.println(ub.toString());
-        System.out.println("所属地：{" + ub.affiliation + "}");
-        System.out.println("カウント数：{" + UserinfoAdd.cnt + "}");
+        System.out.println(ub);
     }
 }
