84e80fc47e3f8582110c91c589dfe06eba1e8850
M-ito0126 <63088691+M-ito0126@users.noreply.github.com>
2020-06-29 16:58:53 +0900

    2回目提出

--- 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
+++ 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
@@ -29,7 +29,7 @@ public class BlackJack {
     public static void select(int t, String sel) {
         //文字入力値がYの場合
         if(sel.equals("Y")) {
-            //追加のカードをランダムに一枚取得
+            //追加のカードを1~10からランダムに一枚取得
             Random rnd = new Random();
             int plasRand = rnd.nextInt(10) + 1;
             //合計に加算
@@ -49,23 +49,21 @@ public class BlackJack {
             //ディーラー側のカードを15~22からランダムに一枚取得
             Random rnd = new Random();
             int dealerRand = rnd.nextInt(8) + 15;
-            //ディーラーのカード（親数値）に加算
-            int dealerCard =+ dealerRand;
-            System.out.println("『親数値：[" + dealerCard + "]　合計：[" + t + "]』");
+            System.out.println("『親数値：[" + dealerRand + "]　合計：[" + t + "]』");
             //親数値が22より大きい場合
-            if(dealerCard > 22) {
+            if(dealerRand > 22) {
                 System.out.println("「WIN」");
             }
             //親数値が合計より大きい場合
-            else if(dealerCard > t) {
+            else if(dealerRand > t) {
                 System.out.println("「LOSE」");
             }
             //親数値が合計と同じ場合
-            else if(dealerCard == t) {
+            else if(dealerRand == t) {
                 System.out.println("「DRAW」");
             }
             //親数値が合計より小さい場合
-            else if(dealerCard < t) {
+            else if(dealerRand < t) {
                 System.out.println("「WIN」");
             }
         }
